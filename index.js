92f15c62a0b69c8d7420d208ebc144c19f6e6027
lindychi <igam0000@gmail.com>
2019-06-24 14:15:58 +0900

    ejs test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -12,3 +14,12 @@ var server = app.listen(app.get('port'), function() {
     console.log("Express server has started on port " + app.get('port'))
 })
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: 'fpeqwfjfashjfehi',
+    resave: false,
+    saveUninitialized: true
+}))
+
+var router = require('./router/main')(app, fs);
--- router/main.js
+++ router/main.js
@@ -1,9 +1,9 @@
-module.exports = function(app)
+module.exports = function(app, fs)
 {
     app.get('/', function(req, res) {
-        res.render('index.html')
-    });
-    app.get('/about', function(req, res) {
-        res.render('about.html')
+        res.render('index', {
+            title: "RFM",
+            length: 5
+        })
     });
 }
