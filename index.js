352c087c6dc688afa7270cfd7a5ccdf575df2fca
lindychi <igam0000@gmail.com>
2019-06-24 14:01:26 +0900

    port change

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
 
-var server = app.listen(3000, function() {
-    console.log("Express server has started on port 3000")
+var server = app.listen(app.get('port'), function() {
+    console.log("Express server has started on port " + app.get('port'))
 })
 
-app.use(express.static('public'));
